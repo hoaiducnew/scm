5baeb68ea5672c5a26890411a6400280b26725c4
Hoai Duc Nguyen <hoaiducnew@gmail.com>
2017-09-27 21:33:02 -0500

    fixed

--- src/main/java/edu/mum/scm/controller/PlayerController.java
+++ src/main/java/edu/mum/scm/controller/PlayerController.java
@@ -1,19 +1,16 @@
 package edu.mum.scm.controller;
 
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-
 import java.io.File;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
